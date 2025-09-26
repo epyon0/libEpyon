faaef63efa7ad54a58d356a83417b69c822dbdc1
epyon0 <epyon0@proton.me>
2025-09-26 11:35:43 -0500

    init commit

---
+++ misc.h
@@ -0,0 +1,23 @@
+#ifndef MISC_H
+#define MISC_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdint.h>
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)   \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0') 
+
+void verbose(const char *msg, const char *file, const int line, const char *func, const bool enabled);
+char* uint32tob(uint32_t i);
+
+#endif
